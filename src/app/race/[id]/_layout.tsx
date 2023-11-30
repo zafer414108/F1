451a4afb08e2e6d4af006fcd7d7f0f9f7e72cc4b
zafer avcı <zaferavc41@zafers-MacBook-Air.local>
2023-12-01 01:04:14 +0300

    first commit

---
+++ src/app/race/[id]/_layout.tsx
@@ -0,0 +1,4 @@
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { ActivityIndicator } from 'react-native';
+
--- src/components/RaceListItem.tsx
+++ src/components/RaceListItem.tsx
@@ -1,86 +1,97 @@
-import { StyleSheet, Text, View, } from 'react-native';
-import { Entypo } from '@expo/vector-icons'; 
-import { Colors } from '../constants/Colors';
-import dayjs from 'dayjs' ;
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import { Colors } from '../Constants/Colors';
+import dayjs from 'dayjs';
+
 import racesResponse from '../../assets/data/races.json';
+import { Link } from 'expo-router';
 const races = racesResponse.data.races.response;
 
+export default function RaceListItem({
+  item,
+  round,
+}: {
+  item: (typeof races)[0];
+  round: number;
+}) {
+  return (
+    <Link href={`/race/${item.id}`} asChild>
+      <Pressable style={styles.itemContainer}>
+        <View style={styles.datesContainer}>
+          <Text style={styles.date}>
+            {dayjs(item.date).subtract(2, 'days').format('DD')}-
+            {dayjs(item.date).format('DD')}
+          </Text>
+          <Text style={styles.month}>{dayjs(item.date).format('MMM')}</Text>
+        </View>
+        <View style={{ flex: 1 }}>
+          <Text style={styles.round}>Round {round}</Text>
+          <Text style={styles.country}>
+            {item.competition.location.country}
+          </Text>
+          <Text style={styles.description}>
+            Formula 1 {item.competition.name} {item.season}
+          </Text>
+        </View>
+        <Entypo name="chevron-right" size={24} color={Colors.primary} />
+      </Pressable>
+    </Link>
+  );
+}
 
-export default function RaceListItem({ item, round }: { item: (typeof races)[0], round:number}) {
-    return(
-    <View style={styles.itemContainer}> 
-   <View style={styles.datesContainer}>
-    <Text style={styles.date}>
-      {dayjs(item.date).subtract(2, 'days').format('DD')}-{dayjs(item.date).format('DD')}</Text>
-    <Text style ={styles.month}>{dayjs(item.date).format('MM')}</Text>
-    </View>
-    <View style={{ flex: 1 }}>
-    <Text style={styles.round}>Round{round}</Text>
-    <Text style={styles.country}>{item.competition.location.country}</Text>
-    <Text style={styles.description}>
-      Formula 1 {item.competition.name}{item.season}
-      </Text>
-      </View>
-    <Entypo name="chevron-right" size={24} color={Colors.primary } />
-  </View>
-    );
-  }
+const styles = StyleSheet.create({
+  itemContainer: {
+    padding: 10,
+    margin: 5,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 10,
 
-
-  const styles = StyleSheet.create({
-    
-    itemContainer:{
-      padding: 10,
-      margin: 5,
-      backgroundColor:'white',
-      flexDirection: 'row',
-      alignItems: 'center',
-      borderRadius: 10,
-      shadowColor: "#000",
-  shadowOffset: {
+    shadowColor: '#000',
+    shadowOffset: {
       width: 0,
       height: 1,
+    },
+    shadowOpacity: 0.18,
+    shadowRadius: 1.0,
+
+    elevation: 1,
   },
-  shadowOpacity: 0.18,
-  shadowRadius: 1.00,
-  
-  elevation: 1,
-    },
-    datesContainer:{
-      padding: 10,
-      marginRight: 10,
-      borderRightWidth: 1,
-      borderColor: 'gainsboro',
-    },
+  datesContainer: {
+    padding: 10,
+    marginRight: 10,
+    borderRightWidth: 1,
+    borderColor: 'gainsboro',
+    alignItems: 'center',
+  },
   date: {
     fontFamily: 'F1-Regular',
   },
-  month:{
+  month: {
     backgroundColor: 'gainsboro',
     paddingVertical: 3,
     paddingHorizontal: 10,
-  
+
     borderRadius: 10,
     overflow: 'hidden',
-  
+
     color: 'dimgray',
     fontWeight: 'bold',
     marginTop: 5,
 
-    fontFamily: 'F1-Bold'
-
+    fontFamily: 'F1-Bold',
   },
-  round:{
-    color: Colors.primary ,
+  round: {
+    color: Colors.primary,
     fontFamily: 'F1-Regular',
   },
-  country:{
+  country: {
     fontSize: 20,
     fontFamily: 'F1-Bold',
     marginVertical: 7,
   },
-  description:{
-    color: 'dimgray'
+  description: {
+    color: 'dimgray',
   },
-  });
-  
+});
